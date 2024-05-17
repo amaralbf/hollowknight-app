ad7e0f058a21e275ddfb057ed4ae36f6d16a0e86
Bruno Amaral <amaralbf@gmail.com>
2024-05-16 22:50:37 -0300

    Zoom on point

--- src/pixi.ts
+++ src/pixi.ts
@@ -4,11 +4,18 @@ import {
   Assets,
   Container,
   FederatedWheelEvent,
+  Graphics,
+  FederatedPointerEvent,
+  Point,
 } from "pixi.js";
 
 const app = new Application();
 let zoomLevel: number;
 let minZoomLevel: number;
+
+const zoomStep = 0.1;
+
+// let mapMousePoint: Point;
 
 export const initCanvas = async () => {
   await app.init({ width: 1800, height: 880, background: "#222" });
@@ -16,36 +23,54 @@ export const initCanvas = async () => {
   document.getElementById("pixijs-canvas")?.appendChild(app.canvas);
 
   const rootContainer = new Container();
-  rootContainer.eventMode = "static";
-  // rootContainer.hitArea = app.screen;
-
   app.stage.addChild(rootContainer);
 
+  rootContainer.eventMode = "static";
+
   const map = await createMap();
-  const zote = await createZote();
 
   rootContainer.addChild(map);
-  rootContainer.addChild(zote);
 
   console.log("map.width", map.width);
+  console.log("map.height", map.height);
   console.log("app.renderer.width", app.renderer.width);
-  console.log(app.renderer.width / map.width);
-  console.log((app.renderer.width / map.width).toFixed(1));
+  // console.log(app.renderer.width / map.width);
+  // console.log((app.renderer.width / map.width).toFixed(1));
 
   const minXZoomLevel = app.renderer.width / map.width;
   const minYZoomLevel = app.renderer.height / map.height;
-  console.log("minXZoomLevel", minXZoomLevel);
-  console.log("minYZoomLevel", minYZoomLevel);
+  // console.log("minXZoomLevel", minXZoomLevel);
+  // console.log("minYZoomLevel", minYZoomLevel);
 
   minZoomLevel = Number(Math.min(minXZoomLevel, minYZoomLevel).toFixed(1));
   console.log("minZoomLevel", minZoomLevel);
 
+  const maskGraphics = new Graphics();
+  maskGraphics.fill();
+  maskGraphics.rect(0, 0, rootContainer.width, rootContainer.height);
+  maskGraphics.fill();
+  rootContainer.addChild(maskGraphics);
+  rootContainer.mask = maskGraphics;
+
   zoomLevel = minZoomLevel;
   rootContainer.scale.set(zoomLevel);
-
   rootContainer.x = (app.renderer.width - rootContainer.width) / 2;
+  rootContainer.y = (app.renderer.height - rootContainer.height) / 2;
 
-  createZoomListener(rootContainer);
+  addMouseClickListener(rootContainer);
+  addZoomListener(rootContainer, map);
+
+  // rootContainer.on("pointerdown", (event: FederatedPointerEvent) => {
+  //   console.log("x:", event.x, ", y:", event.y);
+  //   // mouseposition = mouseposition || { x: 0, y: 0 };
+  //   // mouseposition.x = event.global.x;
+  //   // mouseposition.y = event.global.y;
+  // });
+
+  const charm = await drawCharm("fury_of_the_fallen");
+  rootContainer.addChild(charm);
+
+  // map.x = -300;
 };
 
 const createMap = async () => {
@@ -57,65 +82,98 @@ const createMap = async () => {
   return map;
 };
 
-const createZote = async () => {
-  const texture = await Assets.load("zote.png");
-  const zote = new Sprite(texture);
+const drawCharm = async (id: string) => {
+  const texture = await Assets.load(`src/assets/images/${id}.png`);
+  const element = Sprite.from(texture);
+  element.x = 100;
+  element.y = 100;
 
-  zote.x = app.renderer.width / 2;
-  zote.y = app.renderer.height / 2;
-
-  // Rotate around the center
-  zote.anchor.x = 0.5;
-  zote.anchor.y = 0.5;
-
-  // Listen for frame updates
-  app.ticker.add(() => {
-    zote.rotation += 0.03;
-  });
-
-  return zote;
+  return element;
 };
 
-const createZoomListener = (container: Container) => {
+// const createZote = async () => {
+//   const texture = await Assets.load("zote.png");
+//   const zote = new Sprite(texture);
+
+//   zote.x = app.renderer.width / 2;
+//   zote.y = app.renderer.height / 2;
+
+//   // Rotate around the center
+//   zote.anchor.x = 0.5;
+//   zote.anchor.y = 0.5;
+
+//   // Listen for frame updates
+//   app.ticker.add(() => {
+//     // zote.x = 4000;
+//     zote.rotation += 0.03;
+//   });
+
+//   return zote;
+// };
+
+const addMouseClickListener = (container: Container) => {
+  // container.on("pointermove", (event: FederatedPointerEvent) => {
+  //   // mapMousePoint = event.getLocalPosition(container);
+  //   // console.log(mapMousePoint);
+  // });
+
+  container.on("pointerdown", (event: FederatedPointerEvent) => {
+    console.log("global click:", { x: event.x, y: event.y });
+    console.log("local click:", event.getLocalPosition(container));
+  });
+};
+
+const addZoomListener = (container: Container, map: Sprite) => {
   container.on("wheel", (event: FederatedWheelEvent) => {
     const delta = Math.sign(event.deltaY);
-    console.log("mouse wheel delta", delta);
+    // console.log("mouse wheel delta", delta);
+    const localPoint = event.getLocalPosition(container);
+    // console.log(
+    //   `zooming centered on local point (${localPoint.x}, ${localPoint.y})`,
+    // );
+
+    const globalX = event.x - container.x;
+    const globalY = event.y - container.y;
+    // console.log(`zooming centered on global point (${globalX}, ${globalY})`);
     if (delta > 0) {
-      zoomOut(container);
+      zoom(container, map, localPoint, -zoomStep);
     } else {
-      zoomIn(container);
+      // zoomIn(container, map, { x: globalX, y: globalY });
+      zoom(container, map, localPoint, zoomStep);
     }
   });
 };
 
-function zoomIn(container: Container) {
-  if (zoomLevel.toPrecision(1) === "1") return;
-  zoomLevel += 0.1;
+function zoom(
+  container: Container,
+  map: Sprite,
+  point: Point,
+  zoomStep: number,
+) {
+  if (zoomStep > 0) {
+    if (zoomLevel.toPrecision(1) === "1") return;
+  } else {
+    if (zoomLevel.toPrecision(1) === minZoomLevel.toPrecision(1)) return;
+  }
+
+  const prevZoomLevel = zoomLevel;
+  zoomLevel += zoomStep;
+  const factor = (zoomLevel - prevZoomLevel) / zoomLevel;
+  const offsetX = factor * point.x;
+  const offsetY = factor * point.y;
+  map.x -= offsetX;
+  map.y -= offsetY;
   container.scale.set(zoomLevel);
-  console.log("zoomLevel", zoomLevel.toPrecision(1));
+
+  console.log("zoom", {
+    level: zoomLevel.toPrecision(1),
+    x: point.x,
+    y: point.y,
+  });
+  console.log("container", {
+    x: container.x,
+    y: container.y,
+    width: container.width,
+    height: container.height,
+  });
 }
-
-function zoomOut(container: Container) {
-  if (zoomLevel.toPrecision(1) === minZoomLevel.toPrecision(1)) return;
-  zoomLevel -= 0.1;
-  container.scale.set(zoomLevel);
-
-  console.log("zoomLevel", zoomLevel.toPrecision(1));
-}
-
-// function zoomToPoint(app: Application, container: Container, point) {
-//   const localPoint =
-//     app.renderer.plugins.interaction.mouse.getLocalPosition(container);
-
-//   const newScale = container.scale.x * 1.1;
-//   container.scale.set(newScale);
-
-//   const newPosition = {
-//     x: container.position.x - localPoint.x * (newScale - container.scale.x),
-//     y: container.position.y - localPoint.y * (newScale - container.scale.y),
-//   };
-
-//   container.position.set(newPosition.x, newPosition.y);
-// }
-
-// Function to zoom out
